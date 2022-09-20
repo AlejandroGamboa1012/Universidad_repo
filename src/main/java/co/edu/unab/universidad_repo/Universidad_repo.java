b9616ee5ac0b6d4c55944fa2f28f8cf116416b76
AlejandroGamboa1012 <luisalejandro.gamboa@gmail.com>
2022-09-19 21:22:27 -0500

    segundo intento

---
+++ src/main/java/co/edu/unab/universidad_repo/Universidad_repo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package co.edu.unab.universidad_repo;
+
+/**
+ *
+ * @author FamiliaGamboaBecerra
+ */
+public class Universidad_repo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
